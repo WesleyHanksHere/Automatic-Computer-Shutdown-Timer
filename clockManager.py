dbf0dceec62055cb76c1039f8be32e1d9ad8d575
WesleyHanksHere <wesleybhanks@gmail.com>
2024-08-19 22:01:54 -0600

    Update clockManager.py
    Removed a couple of variables and pieces of code that are unnecessary in this particular use case of clockManager

--- clockManager.py
+++ clockManager.py
@@ -1,28 +1,20 @@
-from win11toast import toast
 import os, time, sys
 
 
 class ClockSystem:
 
     def __init__(self):
-        self.alarms = {}
         self.timers = {}
         self.lastRecordedTime = int(time.time())
     
     def tick(self):
         if self.lastRecordedTime < int(time.time()):
 
-            if self.alarms != {}:
-                for key in self.alarms.keys():
-                    if self.alarms[key] == time.localtime(int(time.time())):
-                        del(self.alarms[key])
-
             if self.timers != {}:
                 for key in self.timers.keys():
                     self.timers[key] -= int(time.time()) - self.lastRecordedTime
                     if self.timers[key] <= 0:
                         del(self.timers[key])
-                        toast("Timer Elapsed")
                         break
             self.lastRecordedTime = int(time.time())
     
